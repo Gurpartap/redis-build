3eff57ed18fdeac36006a8483ad07b8a4234fe41
Gurpartap Singh <contact@gurpartap.com>
2014-09-14 22:56:48 +0530

    Ignore errors for docker rmi

--- script/clean.sh
+++ script/clean.sh
@@ -2,5 +2,5 @@
 # Usage: ./clean.sh
 
 docker rm -f redis-build 2> /dev/null
-docker rmi -f gurpartap/redis-build
-docker rmi -f gurpartap/redis
+docker rmi -f gurpartap/redis-build 2> /dev/null
+docker rmi -f gurpartap/redis 2> /dev/null
